18a79680734ada41b8d612072528c8e0f530853e
dima <shifting.planes@gmail.com>
2017-02-06 15:44:21 -0500

    init project

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.post('/api', function(req,res){
+  console.log(req.body);
+  console.log(req.body.user_email);
+})
+
+app.get('/dog',function(req,res){
+  res.send("I aint no cat");
+})
+
+app.use(function(req,res){
+  //res.render('404',{domain: process.env.DOMAIN});
+  res.send("yo!");
+});
+
+
+var appPort=7777;
+
+app.listen(appPort, function () {
+  console.log("Magic on port %d",appPort);
+});
